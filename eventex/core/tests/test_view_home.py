ba2c74e2eb5e37275bf0117fec6655241d2b401b
joeccosta <joe.dfq@gmail.com>
2021-09-26 11:05:14 -0300

    upgrade requeriments

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -38,6 +38,6 @@ class HomeTest(TestCase):
         expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
 
-    def test_talks_list(self):
+    def test_talks_link(self):
         expected = 'href="{}"'.format(r('talk_list'))
         self.assertContains(self.response, expected)
