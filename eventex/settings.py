cc55c15d49aafc42b191f8046fca5bf0740b7280
joeccosta <joe.dfq@gmail.com>
2021-09-26 11:59:02 -0300

    upgrade requirements, python3.7.12, fix apps cause upgrade django

--- eventex/settings.py
+++ eventex/settings.py
@@ -142,3 +142,5 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
